40649d8a5f13ba08164d0dbac7a69e26b9f2b02e
NeaMary <neamarysm@gmail.com>
2023-06-26 18:30:43 +0300

    main

--- src/main/java/exceptions_1/Main.java
+++ src/main/java/exceptions_1/Main.java
@@ -3,21 +3,26 @@ package exceptions_1;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        T_1 test = new T_1();
-        test.doSomething();
+    public static void main(String[] args){
+//        T_1 test = new T_1();
+//        test.doSomething();
+//
+//        try(Counter cnt = new Counter()) {
+//            cnt.add();
+//            cnt.add();
+//            cnt.add();
+//            System.out.println(cnt.counter);
+//            cnt.close();
+//            cnt.add();
+//        } catch (IOException e){
+//            System.out.println("Error");
+//        }
 
-        try(Counter cnt = new Counter()) {
-            cnt.add();
-            cnt.add();
-            cnt.add();
-            System.out.println(cnt.counter);
-            cnt.close();
-            cnt.add();
-        } catch (IOException e){
-            System.out.println("Error");
-        }
-
-
+        ExceptionTypes ext = new ExceptionTypes();
+        ext.printElement(5);
+        ext.printElement(10);
+        String msg = "null";
+        System.out.println("ext.substring(msg, 2,4) = " + ext.substring(msg, 3, 2));
+        ext.showDate();
     }
 }
